e36b745ce0c687e0416f56ce6982ecd9b5ea482e
Rodrigo Villar Valenca <rodrigovillarvalenca@gmail.com>
2022-02-18 18:28:50 -0300

    req 9 teoricamente ok, saberei na hora de fazer o 9

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -9,7 +9,7 @@ class SolarSystem extends React.Component {
       <div data-testid="solar-system">
         <Title headline="Planetas" />
         {planets.map((planeta) => (<PlanetCard
-          Key={ planeta.name }
+          key={ planeta.name }
           planetName={ planeta.name }
           planetImage={ planeta.image }
         />))}
